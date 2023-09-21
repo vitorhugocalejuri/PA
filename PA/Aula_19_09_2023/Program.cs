47971d675edc58a9f6582d2dffdfdec6c3810909
Aluno <aluno@etec.sp.gov.br>
2023-09-21 20:46:45 -0300

    Atividades Aula 20/09/2023

--- PA/Aula_19_09_2023/Program.cs
+++ PA/Aula_19_09_2023/Program.cs
@@ -6,7 +6,7 @@ namespace Aula_19_09_2023
     {
         static void Main(string[] args)
         {
-            float nota1, nota2, nota3, nota4;
+            float nota1, nota2, nota3, nota4, soma, media;
 
             Console.WriteLine("Hello Master!");
 
@@ -21,6 +21,36 @@ namespace Aula_19_09_2023
 
             Console.Write("Informe a 4° nota: ");
             nota4 = float.Parse(Console.ReadLine());
+
+            soma = nota1 + nota2 + nota3 + nota4;
+            media = soma / 4;
+            Console.WriteLine("A media é " + media);
+         
+
+            //0.00 a 4.99 - Reprovado
+            //5.00 a 6.99 - Recuperação
+            //7.00 a 10 - Aprovado
+
+            if (media <5) 
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Poxa que Pena :( !!!");
+            }
+            else if ( media <7)
+            {
+                Console.WriteLine("Recuperação");
+                Console.WriteLine("Coragem !!!");
+            }
+            else 
+            {
+                Console.WriteLine("Aprovado");
+                Console.WriteLine("Parabens !!!");
+            }
+            // if == se 
+            // else == senão
+
+            // if faz teste condicional apenas com true or false <> sinais de comparação (==) sinal de igualdade (=) atribuição
+            // uso das {} só é necessário caso haja mais que uma linha de instrução após o if e else
 
         }
     }
