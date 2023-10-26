2bf8e1a8b2b4ca1061c3ad81579227d66162fafc
Aluno <aluno@etec.sp.gov.br>
2023-10-26 20:49:11 -0300

    Atividade Aula 26_10_2023 Ex 01 e 02 While e Do While

---
+++ PA/Aula_26_10_2023_ Ex02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula_26_10_2023__Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|-----------------------|");
+            Console.WriteLine("|--- Atividade Ex 02 ---|");
+            Console.WriteLine("|-----------------------|");
+
+            float valorFinanciado, valorParcela;
+            int qtdeParcela, contador = 1;
+
+            Console.Write("Informe o valor Financiado: ");
+            valorFinanciado = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a quantidade de Parcelas: ");
+            qtdeParcela = int.Parse(Console.ReadLine());
+
+            valorParcela = valorFinanciado / qtdeParcela;
+
+            DateTime data = DateTime.Now.Date;
+
+            while (contador <= qtdeParcela)
+            {
+                //Console.WriteLine($"{contador}º parcela: {valorFinanciado / qtdeParcela}"); outras maneiras de fazer o calculo
+                //Console.WriteLine($"{contador}ª parcela: {valorParcela:0.00}");
+                 Console.WriteLine($"{contador}ª parcela ({data}): {qtdeParcela:0.00}");
+                //Console.WriteLine($"{contador}ª parcela: {valorFinanciado / qtdeParcela:0.00}");
+                contador++;
+                data = data.AddMonths(1);
+            }
+        }
+    }
+}
