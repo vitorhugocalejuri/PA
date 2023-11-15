2caa0a58d6a657519217f7cd1dbc2df13491bd24
Aluno <aluno@etec.sp.gov.br>
2023-11-14 22:49:01 -0300

    Aula 14/11/2023 Exercícios

---
+++ PA/Aula 14_11_2023 Exercicio_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("|=================|");
+            Console.WriteLine("|    SUBTRAÇÃO    |");
+            Console.WriteLine("|=================|");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int i = 1; i <=3; i++)
+            {
+
+                Console.Write("Digite um Número: ");
+                num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite outro Número: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"A subtração dos números é: {num1 - num2}");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(); 
+            }
+
+            
+
+        }
+    }
+}
---
+++ PA/Aula 14_11_2023 Exercicio_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aula_14_11_2023_Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("|===================================|");
+            Console.WriteLine("|    NÚMEROS IMPARES EM VERMELHO    |");
+            Console.WriteLine("|===================================|");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Insira um número: ");
+            num = int.Parse(Console.ReadLine());
+
+
+            for (int contador = 0; contador <= num; contador++)
+            {
+
+                if (contador % 2 == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Os números Impares são: {contador} ");
+                } 
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Os números Impares são: {contador} ");
+                    
+                }   
+
+            }
+            
+        }
+    }
+}
