0d60452a0b2856ecb67d3a5ccdf7b5dd27ad22b5
Aluno <aluno@etec.sp.gov.br>
2023-11-21 22:44:07 -0300

    Aula 21/11/2023 Exs

---
+++ PA/Aula 21_11_2023 Ex01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Aula_21_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float maiorAltura = 0, menorAltura = 0;
+            int contador = 1;
+
+            while (contador <= 15)
+            {
+                contador++;
+
+                Console.Write($"Informe a sua Altura:  ");
+                altura = float.Parse(Console.ReadLine());
+
+                if (altura > maiorAltura)
+                    maiorAltura = altura;
+                 
+                if (altura <= menorAltura || menorAltura == 0 )
+                    menorAltura = altura;
+                                            
+            }
+            Console.WriteLine($"A menor altura é: {maiorAltura} ");
+            Console.WriteLine($"A menor altura é: {menorAltura} ");
+
+            // Usando o For
+
+            /*
+             float maiorAltura = 0;
+             float menorAltura = 0;
+
+            for (int contador = 1; contador  <= 15 ; contador ++)           
+            {
+                contador++;
+
+                Console.Write($"Informe a sua Altura:  ");
+                altura = float.Parse(Console.ReadLine());
+
+                if (altura > maiorAltura)
+                    maiorAltura = altura;
+
+                if (altura <= menorAltura || menorAltura == 0)
+                    menorAltura = altura;
+
+            }
+
+            Console.WriteLine($"A menor altura é: {maiorAltura} ");
+            Console.WriteLine($"A menor altura é: {menorAltura} ");
+            */
+
+        }
+    }
+}
