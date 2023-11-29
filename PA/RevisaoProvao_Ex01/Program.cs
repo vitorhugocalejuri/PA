19fa0c230da6ebdba901436c4a993b7c0bf4321b
Aluno <aluno@etec.sp.gov.br>
2023-11-28 22:42:44 -0300

    Aula 28_11_2023 Revisao Provao

---
+++ PA/RevisaoProvao_Ex01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RevisaoProvao_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número inteiro: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0 && numero <= 10)
+            {
+                //Fazer tabuada do 5
+
+                for (int contador = 1; contador <= 10; contador++)
+                {
+                    Console.WriteLine($"5 x {contador} = {5 * contador}");
+                }
+            }
+
+            else
+            {   
+                 
+                //Fazer tabuada do 8
+                for (int contador = 1; contador <= 10; contador++)
+                {
+                    Console.WriteLine($"8 x {contador} = {8 * contador}");
+                }
+            }
+        }
+    }
+}
